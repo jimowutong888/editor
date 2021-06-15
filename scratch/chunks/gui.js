2d57b349148d50970c8d1a568660689112f65fe0
寂寞酱 <63185382+jimowutong888@users.noreply.github.com>
2021-06-15 16:37:31 +0800

    Update gui.js

--- scratch/chunks/gui.js
+++ scratch/chunks/gui.js
@@ -147,7 +147,7 @@ function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "functi
 
 
 var onClickLogo = function onClickLogo() {
-  window.location = '/';
+  window.location = 'https://jimowutong888.github.io/';
 };
 
 var handleTelemetryModalCancel = function handleTelemetryModalCancel() {
